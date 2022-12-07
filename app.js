99d00b56c84c6053783bc545ebfea974f1e6c2e4
agustintrossero <agustintrossero@gmail.com>
2022-12-07 13:14:45 +0100

    creacion de vista Register, html completo

--- app.js
+++ app.js
@@ -8,3 +8,6 @@ app.use(express.static('public'));
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
+app.get('/register', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
