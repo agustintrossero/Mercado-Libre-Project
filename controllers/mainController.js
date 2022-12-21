73d37ab55df235d908b3b8211fd722d5cf8a673c
agustintrossero <agustintrossero@gmail.com>
2022-12-21 12:58:30 +0100

    creacion de la vista results.ejs que responde al formulario de busqueda searchBar y trae el producto buscado por nombre, falta mejorarlo

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,6 +1,24 @@
+const fs = require("fs")
+const path = require("path")
+
+const productsPath = path.join(__dirname, "../data/productsDataBase.json")
+const products = JSON.parse(fs.readFileSync(productsPath, "utf-8"))
+
 const mainController = {
     home: (req,res) =>{
         res.render("home")
+    },
+
+    search: (req,res) =>{
+
+        let busqueda = req.query.searchBar
+       // console.log(busqueda)
+        for( i=0; i<products.length; i++){
+            if(products[i].name.includes(busqueda)){
+                res.render("results", {"searchResult": products[i]})
+             //   console.log(products[i])
+            }
+        }
     }
 }
 
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -4,7 +4,10 @@ const router = express.Router();
 const productController = require("../controllers/productController")
 
 router.get("/products", productController.allProducts)
+
 router.get("/products/create/", productController.create)
+//router.put("/products", productController.store)
+
 router.get("/products/detail/:id", productController.detail)
 
 module.exports = router
